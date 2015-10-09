afc2cd5f3910f84f7fc686169bab6ba61cbe3d93
Wade Fletcher <wadefletcher@Wades-MacBook-Pro.local>
2015-10-08 20:17:48 -0400

    added procfile

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'shh! it\'s a secret!'
-    MONGOALCHEMY_DATABASE = 'location'
+    MONGOALCHEMY_CONNECTION_STRING = os.environ.get('MONGOLAB_URI')
 
     @staticmethod
     def init_app(app):
